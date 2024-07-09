91eeb9cc97248894eb54833560ea03cbaf35e862
gavintaylor <gavin@able-group.co.uk>
2024-07-09 11:18:06 +0100

    Added simplistic back-off for Intacct commits

--- Intacct.SDK/OnlineClient.cs
+++ Intacct.SDK/OnlineClient.cs
@@ -13,8 +13,10 @@
  * permissions and limitations under the License.
  */
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Intacct.SDK.Exceptions;
 using Intacct.SDK.Functions;
 using Intacct.SDK.Xml;
 using Intacct.SDK.Xml.Response;
@@ -39,21 +41,45 @@ namespace Intacct.SDK
         /// <returns></returns>
         public async Task<OnlineResponse> Execute(IFunction apiFunction, RequestConfig requestConfig = null)
         {
-            if (requestConfig == null)
-            {
-                requestConfig = new RequestConfig();
-            }
+            return await ExecuteInternal(apiFunction, requestConfig, 1);
+        }
 
-            List<IFunction> apiFunctions = new List<IFunction>
+        private async Task<OnlineResponse> ExecuteInternal(IFunction apiFunction, RequestConfig requestConfig, int attempts)
+        {
+            try
+            {
+                if (requestConfig == null)
+                {
+                    requestConfig = new RequestConfig();
+                }
+
+                List<IFunction> apiFunctions = new List<IFunction>
             {
                 apiFunction
             };
 
-            OnlineResponse response = await this.ExecuteOnlineRequest(apiFunctions, requestConfig).ConfigureAwait(false);
+                OnlineResponse response = await this.ExecuteOnlineRequest(apiFunctions, requestConfig).ConfigureAwait(false);
 
-            response.Results[0].EnsureStatusSuccess();
+                response.Results[0].EnsureStatusSuccess();
 
-            return response;
+                return response;
+            }
+            catch(ResponseException rex)
+            {
+                if (rex.Message.Contains("GW-0010"))
+                { // Rate limit
+                    // too many goes, just abort
+                    if (attempts >= 5) throw;
+
+                    await Task.Delay((attempts + 1) * 1000);
+                    Console.WriteLine($"Rate limit blocked {attempts} time(s)");
+                    return await ExecuteInternal(apiFunction, requestConfig, attempts + 1);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
         /// <summary>
